552e98d7cddbcf0ae0d31f36c434490446af27ea
maekawa-syuki <syu54117123>
2022-04-09 10:43:35 +0900

    add template

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+
+import '../styles/globals.css'
+import Head from 'next/head'
+import { AppProps } from 'next/app'
+import { AnimatePresence } from 'framer-motion'
+
+export default function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      {/* <AnimatePresence exitBeforeEnter initial={false} > */}
+          <Component {...pageProps} key={router.route} />
+      {/* </AnimatePresence> */}
+    </>
+  )
+}
