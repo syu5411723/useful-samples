65123ef4c5c4059c1def822857f9e7cd468edf76
maekawa-syuki <syu54117123>
2022-04-24 21:56:18 +0900

    te

--- components/design/header/atoms/MenuBG.tsx
+++ components/design/header/atoms/MenuBG.tsx
@@ -3,17 +3,28 @@ import {motion} from 'framer-motion'
 import styled from 'styled-components'
 
 
-const Container = styled(motion.div)``
+const Container = styled(motion.div)`
+    position:fixed;
+    width:400px;
+    height:100vh;
+    top:0;
+    right:0;
+    z-index:3;
+    background-color: rgb(255,188,112);
+`
 const containerV = {
-    closed:{},
-    open:{},
+    closed:{x:"400px", },
+    open:{x:"0px", transition:{duration:0.3}}
 }
 
 
 export const MenuBG = () => {
     return (
-        <div>
+        <Container
+            variants={containerV}
+            animate={isOpen ? "open" : "closed"}
+        >
             
-        </div>
+        </Container>
     )
 }
---
+++ components/design/header/atoms/MenuBgContent.tsx
@@ -0,0 +1,29 @@
+﻿import { motion } from 'framer-motion'
+import {VFC} from 'react'
+import {Link} from "react-router-dom"
+import styled from 'styled-components'
+
+const Data = ["","","","",]
+
+const Container = styled(motion.div)`
+    
+`
+const MenuList = styled(motion.ul)``
+const MenuItem = styled(motion.li)``
+
+const itemV = {
+    closed: { x: "50px", opacity: 0 },
+    open: { x: "0px", opacity: 1 },
+    hover: { scale: 1.2, originX: 0 }
+}
+
+const Links = styled(Link)``
+export const MenuBgContent = () => {
+    return (
+        <Container>
+            <MenuList>
+                {Data.map(data => <MenuItem>{data}</MenuItem>)}
+            </MenuList>
+        </Container>
+    )
+}
