d69e754d6b58db2d8f97fbe9995cc3908b427488
maekawa-syuki <syu54117123>
2022-05-14 08:39:28 +0900

    add page

--- components/pages/InfinitLiquid.tsx
+++ components/pages/InfinitLiquid.tsx
@@ -1,19 +1,57 @@
-﻿import styled from "styled-components"
+﻿import styled, { keyframes } from "styled-components"
 
+const animate = keyframes`
+    0% {
+        filter: hue-rotate(0deg);
+        transform: rotate(0deg)
+    }
+    25% {
+        transform: rotate(15deg)
+        }
+    50% {
+        transform: rotate(0deg)
+    }
+    75% {
+        transform: rotate(-15deg)
+    }
+    100% {
+        filter: hue-rotate(360deg);
+        transform: rotate(0deg)
+    }
+`
+const animateLiquid = keyframes`
+    0% {
+        transform: rotate(0deg)
+    }
+    25% {
+        transform: rotate(-20deg)
+        }
+    50% {
+        transform: rotate(0deg)
+    }
+    75% {
+        transform: rotate(20deg)
+    }
+    100% {
+        transform: rotate(0deg)
+    }
+`
 const Container = styled.section`
     display: flex;
     justify-content: center;
     align-items: center;
     min-height: 100vh;
-    background: #333;
+    background: #121212;
 `
 const Bowl = styled.div`
     position: relative;
-    width: 100px;
-    height: 100px;
+    width: 300px;
+    height: 300px;
     background: rgba(255,255,255,0.1);
     border-radius: 50%;
     border : 8px solid transparent;
+    animation: ${animate} 5s linear infinite;
+    transform-origin: bottom center;
     &:before {
         content: "";
         position: absolute;
@@ -22,25 +60,25 @@ const Bowl = styled.div`
         border: 15px solid #444;
         border-radius: 50%;
         left: 50%;
-        top: --15px;
+        top: -15px;
         transform: translateX(-50%);
         box-shadow: 0 10px #222;
     }
     &:after {
+        content: "";
         position: absolute;
         left: 50%;
         top: 40%;
-        width: 15px;
+        width: 150px;
         height:80px;
         border-radius: 50%;
         transform: translate(-50%, -50%);
-        box-shadow: 0 10px #222;
         background: rgba(255,255,255,0.05);
     }
 `
 const Liquid = styled.div`
     position: absolute;
-    top: 50px;
+    top: 50%;
     left: 5px;
     right: 5px;
     bottom: 5px;
@@ -48,13 +86,38 @@ const Liquid = styled.div`
     border-bottom-left-radius: 150px;
     border-bottom-right-radius: 150px;
     filter:drop-shadow(0 0 80px #41c1fb);
+    animation: ${animateLiquid} 5s linear infinite;
+    transform-origin: top center;
+    &:before{
+        content: "";
+        position: absolute;
+        top: -10px;
+        width: 100%;
+        height:20px;
+        background: #1fa4e0;
+        border-radius: 50%;
+        filter: drop-shadow(0 0 80px #1fa4e0);
+    }
+`
+const Shadow = styled.div`
+    position: absolute;
+    top: calc(50% + 150px);
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+    height: 30px;
+    background:rgba(0,0,0,0.5);
+    border-radius: 50%;
+
 `
 
 export const InfinitLiquid = () => {
     return (
         <Container>
-            <Bowl />
-            <Liquid />
+            <Shadow />
+            <Bowl>
+                <Liquid />
+            </Bowl>
         </Container>
     )
 }
