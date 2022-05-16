e0077b36f71a39f7fe527d1ee9ce90e897e7718e
maekawa-syuki <syu54117123>
2022-05-16 16:54:21 +0900

    add page

---
+++ components/pages/3Dtext.tsx
@@ -0,0 +1,40 @@
+﻿import styled from "styled-components"
+
+const Container = styled.section`
+    background-color: #000;
+    color: #fff;
+    min-height:100vh;
+    display: grid;
+    place-items: center;
+`
+const Banner = styled.div`
+    display:flex;
+`
+const Panel = styled.div`
+    position:relative;
+    width: 100px;
+    height: 100px;
+    border: 1px solid white;
+    overflow: hidden;
+    &:before {
+        position: absolute;
+        content: "";
+        font-size: 100px;
+        width:max-content;
+    }
+
+`
+
+export const ThreeDtext = () => {
+    return (
+        <Container>
+            <Banner />
+            <Panel />
+            <Panel />
+            <Panel />
+            <Panel />
+            <Panel />
+
+        </Container>
+    )
+}
