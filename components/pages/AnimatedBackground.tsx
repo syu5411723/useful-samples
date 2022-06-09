57c6b1e0f882baa2aac8828a5ebc244648607e96
maekawa-syuki <syu54117123>
2022-06-09 17:38:12 +0900

    add page

---
+++ components/pages/AnimatedBackground.tsx
@@ -0,0 +1,19 @@
+﻿import React from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAlien } from '@fortawesome/free-regular-svg-icons'
+
+const Container = styled.div`
+    overflow:hidden;
+    min-height:100vh;
+    background:#000;
+`
+
+export const AnimatedBackground = () => {
+    return (
+        <>
+        <FontAwesomeIcon icon={faRss} />
+        <FontAwesomeIcon icon={faAngry} />
+        </>
+    )
+}
