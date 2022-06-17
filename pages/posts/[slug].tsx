2f9eda14d0a6c22791e5d2c3731f218d6ad0a3ee
maekawa-syuki <syu54117123>
2022-06-17 14:22:54 +0900

    tes

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -54,6 +54,7 @@ const DetailPage = ({ postData }) => {
             {/* <Dropmorphism /> */}
             {/* <AnimatedBackground /> */}
             <Smokeanimation />
+            
         </>
     )
 }
