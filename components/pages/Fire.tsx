d1fdb9a877980582dd526ec49f141854a450ebb0
maekawa-syuki <syu54117123>
2022-06-19 21:26:09 +0900

    add fire-animation

--- components/pages/Fire.tsx
+++ components/pages/Fire.tsx
@@ -17,22 +17,26 @@ const Text = styled.h2`
     color:#ffeb3b;
     font-weight:600;
     font-size:16em;
-    -webkit-box-reflect:below 120px linear-gradient(transparent,) ;
-    text-shadow:0 0 10px #ff8c3b
-    0 0 40px #ff8c3b
-    0 0 80px #ff8c3b
-    0 0 160px #ff8c3b
+    -webkit-box-reflect:below -95px linear-gradient(transparent, #000100) ;
+    text-shadow:0 0 10px #ff8c3b,
+    0 0 40px #ff8c3b,
+    0 0 80px #ff8c3b,
+    0 0 160px #ff8c3b,
     0 0 240px #ff8c3b
 
     ;
 `
-
+const SVG = styled.svg``
+const Filter = styled.filter``
 export const Fire = () => {
     return (
         <Container>
             <TextWrapper>
                 <Text>Fire</Text>
             </TextWrapper>
+            <SVG>
+
+            </SVG>
 
         </Container>
     )
